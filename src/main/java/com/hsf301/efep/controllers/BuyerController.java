5facac7170429d2879108b3786b92a3bc435a5d7
unknown <vannhuquynhp@gmail.com>
2024-10-17 19:08:07 +0700

    feat: add database

--- src/main/java/com/hsf301/efep/controllers/BuyerController.java
+++ src/main/java/com/hsf301/efep/controllers/BuyerController.java
@@ -1,8 +1,13 @@
 package com.hsf301.efep.controllers;
 
+import com.hsf301.efep.models.request_models.AddToWishListRequest;
 import com.hsf301.efep.serivces.BuyerService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
