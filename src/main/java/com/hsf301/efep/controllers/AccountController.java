0c84b93ba3f1a86bb4ca0263682845f802720828
hai65 <vyastaroth123@gmail.com>
2024-10-17 15:44:34 +0700

    feat: add login

--- src/main/java/com/hsf301/efep/controllers/AccountController.java
+++ src/main/java/com/hsf301/efep/controllers/AccountController.java
@@ -1,8 +1,11 @@
 package com.hsf301.efep.controllers;
 
 import com.hsf301.efep.serivces.AccountService;
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,4 +15,8 @@ public class AccountController {
 
     private final AccountService accountService;
 
+    @PostMapping("/login")
+    public String login(LoginRequest request, HttpSession session, Model model) {
+
+    }
 }
