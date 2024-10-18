ace7f40dcc302aa832b195e390a81eac54e3c5bd
unknown <vannhuquynhp@gmail.com>
2024-10-18 08:36:24 +0700

    feat: add controller for buyer

--- src/main/java/com/hsf301/efep/controllers/BuyerController.java
+++ src/main/java/com/hsf301/efep/controllers/BuyerController.java
@@ -1,14 +1,17 @@
 package com.hsf301.efep.controllers;
 
 import com.hsf301.efep.models.request_models.AddToWishListRequest;
+import com.hsf301.efep.models.request_models.DeleteWishlistItemRequest;
+import com.hsf301.efep.models.request_models.DeleteWishlistRequest;
+import com.hsf301.efep.models.request_models.UpdateWishlistRequest;
 import com.hsf301.efep.serivces.BuyerService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequiredArgsConstructor
@@ -17,8 +20,43 @@ public class BuyerController {
 
     private final BuyerService buyerService;
 
+    //-------------------------VIEW WISHLIST------------------//
+
+    @GetMapping("/wishlist")
+    @Operation(hidden = true)
+    public String viewWishlist(HttpSession session, Model model) {
+        return buyerService.viewWishlist(session, model);
+    }
+
+    //-------------------------ADD TO WISHLIST------------------//
+
     @PostMapping("/wishlist")
-    public String wishlist(AddToWishListRequest request, HttpServletRequest httpServletRequest, Model model, HttpSession session) {
+    public String addToWishlist(AddToWishListRequest request, HttpServletRequest httpServletRequest, Model model, HttpSession session) {
         return buyerService.addToWishList(request, httpServletRequest, session, model);
     }
+
+    //-------------------------UPDATE WISHLIST------------------//
+
+    @PutMapping("/wishlist")
+    @Operation(hidden = true)
+    public String updateWishlist(UpdateWishlistRequest request, HttpSession session, Model model) {
+        return buyerService.updateWishlist(request, session, model);
+    }
+
+    //-------------------------DELETE WISHLIST------------------//
+
+    @DeleteMapping("/wishlist")
+    @Operation(hidden = true)
+    public String deleteWishlist(DeleteWishlistRequest request, HttpSession session, Model model) {
+        return buyerService.deleteWishlist(request, session, model);
+    }
+
+    //-------------------------DELETE WISHLIST ITEM------------------//
+
+    @DeleteMapping("/wishlist-item")
+    @Operation(hidden = true)
+    public String deleteWishlistItem(DeleteWishlistItemRequest request, HttpSession session, Model model) {
+        return buyerService.deleteWishlistItem(request, session, model);
+    }
+
 }
--- src/main/java/com/hsf301/efep/service_implementors/BuyerServiceImpl.java
+++ src/main/java/com/hsf301/efep/service_implementors/BuyerServiceImpl.java
@@ -1,4 +1,43 @@
 package com.hsf301.efep.service_implementors;
 
-public class BuyerServiceImpl {
+import com.hsf301.efep.models.request_models.AddToWishListRequest;
+import com.hsf301.efep.models.request_models.DeleteWishlistItemRequest;
+import com.hsf301.efep.models.request_models.DeleteWishlistRequest;
+import com.hsf301.efep.models.request_models.UpdateWishlistRequest;
+import com.hsf301.efep.serivces.BuyerService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service
+@RequiredArgsConstructor
+public class BuyerServiceImpl implements BuyerService {
+
+    @Override
+    public String viewWishlist(HttpSession session, Model model) {
+        return "";
+    }
+
+    @Override
+    public String addToWishList(AddToWishListRequest request, HttpServletRequest httpServletRequest, HttpSession session, Model model) {
+        return "";
+    }
+
+    @Override
+    public String updateWishlist(UpdateWishlistRequest request, HttpSession session, Model model) {
+        return "";
+    }
+
+    @Override
+    public String deleteWishlist(DeleteWishlistRequest request, HttpSession session, Model model) {
+        return "";
+    }
+
+    @Override
+    public String deleteWishlistItem(DeleteWishlistItemRequest request, HttpSession session, Model model) {
+        return "";
+    }
+
 }
