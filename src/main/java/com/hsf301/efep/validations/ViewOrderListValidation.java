aff4c9b6d7d7e7651220f4ff0d231777b7108df5
QuynhCharisses <vannhuquynhp@gmail.com>
2024-11-05 20:19:14 +0700

    Build: rebuild project

--- src/main/java/com/hsf301/efep/validations/ViewOrderListValidation.java
+++
@@ -1,9 +0,0 @@
-package com.hsf301.efep.validations;
-
-public class ViewOrderListValidation {
-    public static String validate() {
-        String error = "";
-        //code validation here
-        return error;
-    }
-}
