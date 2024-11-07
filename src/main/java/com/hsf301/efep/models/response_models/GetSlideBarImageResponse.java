1ce03c5fc6c1c70817a58b2466177aa0df10700c
QuynhCharisses <vannhuquynhp@gmail.com>
2024-11-08 06:06:25 +0700

    feat: add new request + resposnse for getSlideBar, getSoldTop and add new test result fot getSlideBar + getSoldTop

--- src/main/java/com/hsf301/efep/models/response_models/GetSlideBarImageResponse.java
+++ src/main/java/com/hsf301/efep/models/response_models/GetSlideBarImageResponse.java
@@ -1,4 +1,16 @@
 package com.hsf301.efep.models.response_models;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class GetSlideBarImageResponse {
+    String status;
+    String message;
+    String link1;
+    String link2;
 }
