1609c21b2ec63ffaa6bc7880a89d77dc535cab4a
thanhminhtrit <156803262+thanhminhtrit@users.noreply.github.com>
2024-11-07 14:53:27 +0700

    merge: merge code

--- src/main/java/com/hsf301/efep/service_implementors/AccountServiceImpl.java
+++ src/main/java/com/hsf301/efep/service_implementors/AccountServiceImpl.java
@@ -1,12 +1,5 @@
 package com.hsf301.efep.service_implementors;
 
-import com.hsf301.efep.configurations.ReturnPageConfig;
-import com.hsf301.efep.enums.ActionCaseValues;
-import com.hsf301.efep.enums.Roles;
-import com.hsf301.efep.enums.Status;
-import com.hsf301.efep.models.entity_models.Account;
-import com.hsf301.efep.models.entity_models.User;
-import com.hsf301.efep.models.entity_models.Wishlist;
 import com.hsf301.efep.models.request_models.LoginRequest;
 import com.hsf301.efep.models.request_models.RegisterRequest;
 import com.hsf301.efep.models.response_models.LoginResponse;
@@ -15,14 +8,10 @@ import com.hsf301.efep.repositories.AccountRepo;
 import com.hsf301.efep.repositories.UserRepo;
 import com.hsf301.efep.repositories.WishlistRepo;
 import com.hsf301.efep.services.AccountService;
-import com.hsf301.efep.validations.RegisterValidation;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.time.LocalDate;
-import java.util.ArrayList;
 
 @Service
 @RequiredArgsConstructor
